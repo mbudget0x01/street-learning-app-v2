742768d8c474fc716451eb44cb4aab4bae0cc723
mbudget0x01 <56198597+mbudget0x01@users.noreply.github.com>
2021-07-07 12:07:25 +0200

    added link to github page

---
+++ src/ui/GithubIcon.tsx
@@ -0,0 +1,10 @@
+import githubIcon from '@iconify-icons/mdi/github';
+import Icon from '@iconify/react';
+
+
+//both icons named Icon I need to split
+export const GitHubIcon = () => {
+    return(
+            <Icon icon={githubIcon} />
+    )
+}
