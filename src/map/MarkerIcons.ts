44de75b99236720b7721919994529733b49f918e
mbudget0x01 <56198597+mbudget0x01@users.noreply.github.com>
2021-07-10 12:51:04 +0200

    Added PinpointMarker

---
+++ src/map/MarkerIcons.ts
@@ -0,0 +1,18 @@
+import L from 'leaflet';
+
+const IconGuess = new L.Icon({
+    iconUrl: 'assets/icons/icon_marker_guess.svg',
+    iconSize: [64, 64],
+    iconAnchor: [32, 64],
+    popupAnchor: [0, -60],
+});
+
+const IconPinpoint = new L.Icon({
+    iconUrl: 'assets/icons/icon_marker_center.svg',
+    iconSize: [64, 64],
+    iconAnchor: [32, 64],
+    popupAnchor: [0, -60],
+});
+
+
+export { IconGuess, IconPinpoint};
