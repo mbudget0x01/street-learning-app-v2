bbcf91b288987dc1bc7f04f519ff56f3bed8dff9
mbudget0x01 <56198597+mbudget0x01@users.noreply.github.com>
2021-07-10 19:10:47 +0200

    fixes #34

--- src/map/Street.tsx
+++ src/map/Street.tsx
@@ -1,4 +1,5 @@
-import { LatLngExpression } from 'leaflet';
+import { LatLng, LatLngExpression } from 'leaflet';
+import React from 'react';
 import { Polyline } from 'react-leaflet';
 import IDrawablePolyLine from '../geocode/IDrawablePolyLine';
 import IDrawableStreet from "../geocode/IDrawableStreet";
@@ -11,25 +12,33 @@ interface Props {
 
 export const Street = (props: Props) => {
 
-    if(props.drawableStreet === null || props.drawableStreet === undefined){
+    //needed to prevent subsequent zoom to center
+    const [center, setCenter] = React.useState<LatLng | undefined>(undefined);
+
+    if (props.drawableStreet === null || props.drawableStreet === undefined) {
         //if street nothing don't render
-        return <div/>
+        return <div />
     }
 
-    //center must be instance as per type definition
-    props.onCenterChanged(props.drawableStreet.center)
+    if (center === undefined || center !== props.drawableStreet.center) {
+        //center must be instance as per type definition
+        setCenter(props.drawableStreet.center)
+        props.onCenterChanged(props.drawableStreet.center)
+    }
+
+
 
     if (props.drawableStreet.polyLines !== undefined) {
         //render polylines
-        let i:number = 0;
-        const nextID = (polyLine:IDrawablePolyLine) => {
+        let i: number = 0;
+        const nextID = (polyLine: IDrawablePolyLine) => {
             i++
             return polyLine.wayPoints[0].toString() + i
         }
         return (
             <div>
                 {
-                    props.drawableStreet.polyLines.map((polyLine) =>                        
+                    props.drawableStreet.polyLines.map((polyLine) =>
                         <Polyline pathOptions={{ color: 'red' }} positions={polyLine.wayPoints} key={nextID(polyLine)} />
                     )
                 }
